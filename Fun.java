6d7944123d0a02dc97fb7f73b7dc2b5fb47cfd62
evershinemj <37256562+evershinemj@users.noreply.github.com>
2019-07-30 16:12:24 +0800

    third commit
    edited on github

--- Fun.java
+++ Fun.java
@@ -2,5 +2,6 @@ public class Fun {
 	public static void main(String[] args) {
 		System.out.println("I'm having a lot of fun!");
 		System.out.println("having fun with git!");
+		System.out.println("thought it's so hot today");
 	}
 }
