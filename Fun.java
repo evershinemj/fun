0de65a52a01e5d0d51b6ca2c03ac4cdcfa139017
wangxueming <vndxmg@cn.ibm.com>
2019-07-30 16:20:00 +0800

    fourth commit 	modified:   Fun.java

--- Fun.java
+++ Fun.java
@@ -3,5 +3,6 @@ public class Fun {
 		System.out.println("I'm having a lot of fun!");
 		System.out.println("having fun with git!");
 		System.out.println("thought it's so hot today");
+		System.out.println("git is great!");
 	}
 }
