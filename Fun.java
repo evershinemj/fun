873b80d09bcb8cfc36e2a2f66f7d987284636ac5
wangxueming <vndxmg@cn.ibm.com>
2019-07-31 17:45:30 +0800

    revert

--- Fun.java
+++ Fun.java
@@ -3,21 +3,5 @@ public class Fun {
 		System.out.println("I'm having a lot of fun!");
 		System.out.println("having fun with git!");
 		System.out.println("thought it's so hot today");
-		System.out.println("git is great!");
-		System.out.println("today is for git");
-		System.out.println("I love git!");
-		
-		// edited on branch foo
-		System.out.println("created a new branch foo");
-
-		// to be committed on git gui
-		System.out.println("committed on git gui");
-		
-		// added on remote origin
-		System.out.println("edited on github");
-
-		System.out.println("July 31");
-		System.out.println("less hot today");
-
 	}
 }
